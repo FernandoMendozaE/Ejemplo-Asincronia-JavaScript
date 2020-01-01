c29ba142cc7610cdf10e1b4189bf1721e4c1dfe1
Fernando Mendoza Escobar <ferchome0@gmail.com>
2020-01-01 18:00:38 -0400

    Ejemplo Promise simulando una base de datos y cambiando ejemplo callback2 a promesa

--- promesa.js
+++ promesa.js
@@ -24,5 +24,46 @@ let salarios = [
   }
 ];
 
+/**
+ * Promise getEmpleado
+ */
+let getEmpleado = id => {
+  return new Promise((resolve, reject) => {
+    let empleadoDB = empleados.find(empleado => empleado.id === id);
+    if (!empleadoDB) {
+      reject(`No existe un empleado con el ID ${id}`);
+    } else {
+      resolve(empleadoDB);
+    }
+  });
+};
 
-+
+let getSalario = empleado => {
+  return new Promise((resolve, reject) => {
+    let salarioDB = salarios.find(salario => salario.id === empleado.id);
+    if (!salarioDB) {
+      reject(`No se encontró un salario para el usuario ${empleado.nombre}`);
+    } else {
+      resolve({
+        nombre: empleado.nombre,
+        salario: salarioDB.salario,
+        id: empleado.id
+      });
+    }
+  });
+};
+
+getEmpleado(2)
+  .then(empleado => {
+    // console.log("Empleado de BD", empleado);
+    getSalario(empleado)
+      .then(resp => {
+        console.log(`El salario de ${resp.nombre} es de ${resp.salario}$`);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+  .catch(err => {
+    console.log(err);
+  });
